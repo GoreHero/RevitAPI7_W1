6cbf47295fe5dd5579a6bbb79f4b5848afaca074
Кирилл Зайцев <moja.orbita@ya.ru>
2023-02-08 00:40:36 +0300

    Добавьте файлы проекта.

---
+++ RevitAPI7_W1/Main.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+//здесь запускается окно
+namespace RevitAPI7_W1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RevitAPI7_W1/MainView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Windows;
+
+namespace RevitAPI7_W1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView(ExternalCommandData commandData)
+        {
+            InitializeComponent();
+            MainViewViewModel vm = new MainViewViewModel(commandData);
+            vm.CloseRequest += (s, e) => this.Close();
+            DataContext = vm;
+        }
+    }
+}
